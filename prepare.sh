12baf6439e29f2f934912c4bf2110bddae92a962
Steve HOLWEG <skytep@gmail.com>
2018-01-27 12:37:08 +0100

    Working prepare

--- prepare.sh
+++ prepare.sh
@@ -3,10 +3,11 @@
 
 MARLIN_VER=1.1.8
 
+curl -L https://github.com/MarlinFirmware/Marlin/archive/${MARLIN_VER}.tar.gz -o ${MARLIN_VER}.tar.gz
 
-wget https://github.com/MarlinFirmware/Marlin/archive/${MARLIN_VER}.tar.gz
+tar zxf ${MARLIN_VER}.tar.gz
 
-tar zxvf ${MARLIN_VER}.tar.gz
+rm ${MARLIN_VER}.tar.gz
 
-cp -f Configuration.h ${MARLIN_VER}/Marlin/Configuration.h
-cp -f Configuration_adv.h ${MARLIN_VER}/Marlin/Configuration_adv.h
+cp -f Configuration.h Marlin-${MARLIN_VER}/Marlin/Configuration.h
+cp -f Configuration_adv.h Marlin-${MARLIN_VER}/Marlin/Configuration_adv.h
